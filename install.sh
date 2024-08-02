52d86c2b644d3154a0db6e5cf787c32ae57da3d3
铁铁汁 <9720386+tietiezhi@user.noreply.gitee.com>
2024-08-02 19:42:03 +0800

    update

---
+++ install.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# 函数定义：生成配置文件内容
+generate_config() {
+    local vless_id=$1
+    local vmess_id=$2
+    local api_host=$3
+    local api_key=$4
+
+    # 生成config文件内容
+    config_content="Log:
+  Level: debug
+Nodes:
+  - PanelType: "GoV2Panel"
+    ApiConfig:
+      ApiHost: $api_host
+      ApiKey: $api_key
+      NodeID: $vless_id
+      NodeType: Vless
+      Timeout: 5
+      EnableVless: true
+      VlessFlow: "xtls-rprx-vision"
+      DisableCustomConfig: true
+    ControllerConfig:
+      UpdatePeriodic: 5
+      DNSType: AsIs
+      EnableREALITY: true
+      REALITYConfigs:
+        Show: true
+        Dest: www.microsoft.com:443
+        ServerNames:
+          - www.microsoft.com
+        PrivateKey: MGcbKLA9jtg3SwHviAMH1EVDnYNODWpiz0qdqNvgmHw
+        ShortIds:
+          - bd5c500d0928efd4
+  - PanelType: "GoV2Panel"
+    ApiConfig:
+      ApiHost: $api_host
+      ApiKey: $api_key
+      NodeID: $vmess_id
+      NodeType: Vmess
+      Timeout: 5
+    ControllerConfig:
+      UpdatePeriodic: 5
+"
+
+    # 将内容写入文件
+    echo "$config_content" > "config.yml"
+}
+
+# 主程序
+
+# 检查参数数量
+if [[ $# -ne 4 ]]; then
+    echo "Usage: $0 <Vless节点ID> <Vmess节点ID> <前端面板地址> <前端面板密钥>"
+    exit 1
+fi
+
+# 接收参数
+vless_id=$1
+vmess_id=$2
+api_host=$3
+api_key=$4
+
+# 生成配置文件
+echo "正在根据输入参数生成配置文件！"
+generate_config "$vless_id" "$vmess_id" "$api_host" "$api_key"
+
+# 下载 XrayR2
+echo "下载 XrayR2 中..."
+wget "https://github.com/Tie-Tie/Xrayr-Auto/releases/download/xrayr-auto/XrayR" -q --show-progress
+
+# 赋予执行权限
+echo "赋予XrayR执行权限"
+chmod +x XrayR
+
+echo "正在设置环境变量XRAY_BUF_SPLICE=disable，有效提高流量统计准确率！"
+export XRAY_BUF_SPLICE=disable
+
+# 执行 XrayR
+echo "执行 XrayR..."
+./XrayR
+
+echo "XrayR 执行完成"
